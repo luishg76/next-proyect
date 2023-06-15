043b709956d248e1197908e032a28b52c4929cf4
Luis Hernández Gómez <ing.luishg76@gmail.com>
2023-06-15 12:18:46 -0500

    init-repo(15-06-023)X

---
+++ app/about/page.jsx
@@ -0,0 +1,10 @@
+export default function AboutPage(){
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo quibusdam dolor id totam est. Dolore itaque, enim deserunt omnis corrupti placeat. Minima voluptatum vero eligendi recusandae modi expedita, quod qui?</p>
+            </div>
+    
+    );
+}
+
---
+++ app/services/page.jsx
@@ -0,0 +1,11 @@
+
+
+function ServicesPage() {
+  return (
+    <div>
+        <h1>Services</h1>
+    </div>
+  )
+}
+
+export default ServicesPage;
