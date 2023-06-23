33bad0ff86a77fff9e797bdaa158de9ff756c2f7
Luis Hernández Gómez <ing.luishg76@gmail.com>
2023-06-23 11:41:46 -0500

    salva(23-06-023)X1

--- app/users/UserDetails.jsx
+++ app/users/UserDetails.jsx
@@ -8,26 +8,35 @@ export default function UserDetails({
   return (
     <figure className="sm:flex bg-slate-100 border rounded-xl p-2 sm:p-0 overflow-clip">
       <img
-        className="w-24 sm:w-96 h-24 sm:h-auto rounded-full sm:rounded-none mx-auto"
+        className="w-20 sm:w-96 h-20 sm:h-auto rounded-full sm:rounded-none mx-auto"
         src={avatar}
         alt=""
         width={128}
         height={128}
       ></img>
       <figcaption className="p-2 text-center sm:text-left">
-        <div className="my-2">
-          <p className="text-xl text-slate-900">
-            {first_name + " " + last_name}
+        <div className="mb-2">
+          <p className="text-xs sm:text-xl font-bold text-slate-900">
+            {first_name} {last_name}
           </p>
-          <a className="text-blue-500" href={email}>
+          <a
+            className="text-xs sm:text-sm sm:space-y-4 text-blue-500"
+            href={email}
+          >
             {email}
           </a>
         </div>
-
-        <p className="text-sm font-light space-y-4">{support.text}</p>
-        <a className="text-blue-500" href={support.url}>
-          {support.url}
-        </a>
+        <div>
+          <p className="text-xs sm:text-xl font-light sm:space-y-4">
+            {support.text}
+          </p>
+          <a
+            className="text-xs sm:text-sm sm:space-y-4 text-blue-500"
+            href={support.url}
+          >
+            {support.url}
+          </a>
+        </div>
       </figcaption>
     </figure>
   );
--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
@@ -84,10 +85,12 @@ function Navegation() {
                   <div>
                     <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="sr-only">Open user menu</span>
-                      <img
+                      <Image
                         className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        src="/img/avatar.png"
                         alt=""
+                        width={14}
+                        height={18}
                       />
                     </Menu.Button>
                   </div>
